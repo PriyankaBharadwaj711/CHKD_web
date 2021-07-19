e1ca5d4e83fa2d550e990af7dc2e172b8be3cb1e
adithyar82 <30332935+adithyar82@users.noreply.github.com>
2021-07-19 13:36:07 +0530

    Update module_home.php

--- module_home.php
+++ module_home.php
@@ -5,12 +5,12 @@ if(isset($_POST['search']))
 
  $search_val=$_POST['search_term'];
 	
- $sql ="SELECT * from modules_home where MATCH(title,description) AGAINST('$search_val')";
+ $sql ="SELECT * from add_modules where MATCH(title,description) AGAINST('$search_val')";
  $result = $conn->query($sql);
  if($result->num_rows>0){
  while($row=$result->fetch_assoc())
  {
-  echo "<li><a href='index.php".$row['status']."'><span class='title'>".$row['title']."</span><br><span class='desc'>".$row['description']."</span></a></li>";
+  echo "<li><a href='index.php".$row['status']."'><span class='title'>".$row['title']."</span><br><span class='desc'>".$row['module_id']."</span></a></li>";
  }
  exit();
 }
